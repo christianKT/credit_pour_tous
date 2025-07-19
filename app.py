9d6e44d8befa0d703d94884efe32ece2647c7847
christianKT <christiankuate22@yahoo.com>
2025-07-19 21:28:00 +0200

    commented line

--- app.py
+++ app.py
@@ -155,6 +155,7 @@ def reset():
     session.clear()
     return redirect(url_for("chat"))
 
+# uniquement en local
 # if __name__ == "__main__":
 #     port = int(os.environ.get("PORT", 5000))
 #     app.run(host="0.0.0.0", port=port)
